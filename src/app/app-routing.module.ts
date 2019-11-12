d5c3adda1ff926fb435dad5f0efa44ddb81549c6
Ho Duong <hxhduong4394@gmail.com>
2019-11-12 09:35:00 +0700

    Update Create & Not Null Validate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {UserComponent} from './user/user.component';
+import {UserCreateComponent} from './user-create/user-create.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: UserComponent
   },
+  {
+    path: 'create',
+    component: UserCreateComponent
+  }
 ];
 
 @NgModule({
