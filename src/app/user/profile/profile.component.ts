23a9c9ae56204f090876b748093c04f1d41c945d
Ho Duong <hxhduong4394@gmail.com>
2019-11-27 09:21:21 +0700

    Update User Profile component

---
+++ src/app/user/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../model/user.interface';
+import {Subscription} from 'rxjs';
+import {FormBuilder} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../service/auth.service';
+import {SongService} from '../../service/song.service';
+import {PlayingQueueService} from '../../service/playing-queue.service';
+import {UserService} from '../../service/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  currentUser: User;
+  subscription: Subscription = new Subscription();
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router,
+              private authService: AuthService, private songService: SongService,
+              private playingQueueService: PlayingQueueService, private userService: UserService) {
+    this.userService.getProfile().subscribe(
+      currentUser => {
+        this.currentUser = currentUser;
+      }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+}
