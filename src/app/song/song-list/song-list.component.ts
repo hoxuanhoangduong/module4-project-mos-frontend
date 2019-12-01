497362a4cd6c9cd58d7f6729fcbd76560b4c8dd5
Ho Duong <hxhduong4394@gmail.com>
2019-12-02 06:47:17 +0700

    Emend song-list and add direct to song-detail

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -5,6 +5,8 @@ import {AddSongToPlaylistService} from '../../service/add-song-to-playlist.servi
 import {PlayingQueueService} from '../../service/playing-queue.service';
 import {UserService} from '../../service/user.service';
 import {User} from '../../model/user.interface';
+import {Subscription} from 'rxjs';
+import {Page} from '../../model/page';
 
 @Component({
   selector: 'app-song-list',
@@ -13,11 +15,15 @@ import {User} from '../../model/user.interface';
 })
 export class SongListComponent implements OnInit {
   currentUser: User;
-  private pageNumber: number;
-  private pageSize: number;
-  private totalItems: number;
+  first: boolean;
+  last: boolean;
+  pageNumber = 0;
+  pageSize: number;
+  pages: Page[] = [];
   private message;
   private songList: Song[];
+  loading: boolean;
+  subscription: Subscription = new Subscription();
 
   constructor(private songService: SongService, private addSongToPlaylistService: AddSongToPlaylistService,
               private playingQueueService: PlayingQueueService, private userService: UserService) {
@@ -29,20 +35,44 @@ export class SongListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.songService.getSongList().subscribe(
+    this.loading = true;
+    this.goToPage(this.pageNumber, true);
+  }
+
+  goToPage(i: number, scrollUp?: boolean) {
+    this.subscription.add(this.songService.getSongList(i, undefined).subscribe(
       result => {
         if (result != null) {
+          if (scrollUp) {
+            window.scroll(0, 0);
+          }
           this.songList = result.content;
           this.songList.forEach((value, index) => {
             this.songList[index].isDisabled = false;
           });
+          this.first = result.first;
+          this.last = result.last;
           this.pageNumber = result.pageable.pageNumber;
           this.pageSize = result.pageable.pageSize;
+          this.pages = new Array(result.totalPages);
+          for (let j = 0; j < this.pages.length; j++) {
+            this.pages[j] = {pageNumber: j};
+          }
+          for (const song of this.songList) {
+            this.checkDisabledSong(song);
+          }
         }
       }, error => {
         this.message = 'Cannot retrieve song list. Cause: ' + error.songsMessage;
+      }, () => {
+        // for (const song of this.songList) {
+        //   if (song.loadingLikeButton) {
+        //     song.loadingLikeButton = false;
+        //   }
+        // }
+        this.loading = false;
       }
-    );
+    ));
   }
 
   addToPlaylist(song) {
@@ -53,5 +83,21 @@ export class SongListComponent implements OnInit {
   addToPlaying(song: Song, event) {
     event.stopPropagation();
     this.playingQueueService.addToQueue(song);
+  }
+
+  checkDisabledSong(song: Song) {
+    let isDisabled = false;
+    for (const track of this.playingQueueService.currentQueueSubject.value) {
+      if (song.url === track.link) {
+        isDisabled = true;
+        break;
+      }
+    }
+    song.isDisabled = isDisabled;
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
