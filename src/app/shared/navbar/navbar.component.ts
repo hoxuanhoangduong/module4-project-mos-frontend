c6e8729f2255b5e597ee4182ecade11df2fdd52d
Celano Gelato <ltt2895@gmail.com>
2019-12-03 15:28:50 +0700

    change navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,6 +3,38 @@ import {AuthService} from '../../service/auth.service';
 import {UserService} from '../../service/user.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
+import * as $ from 'jquery';
+import 'bootstrap';
+
+function showLoginForm() {
+  $('#loginModal .registerBox').fadeOut('fast', function registerBox() {
+    $('.loginBox').fadeIn('fast');
+    $('.register-footer').fadeOut('fast', function registerFooter() {
+      $('.login-footer').fadeIn('fast');
+    });
+    $('.modal-title').html('Login with');
+  });
+  $('.error').removeClass('alert alert-danger').html('');
+}
+function showRegisterForm() {
+  $('.loginBox').fadeOut('fast', function loginBox() {
+    $('.registerBox').fadeIn('fast');
+    $('.login-footer').fadeOut('fast', function loginFooter() {
+      $('.register-footer').fadeIn('fast');
+    });
+    $('.modal-title').html('Register with');
+  });
+  $('.error').removeClass('alert alert-danger').html('');
+}
+
+function shakeModal() {
+  $('#loginModal .modal-dialog').addClass('shake');
+  $('.error').addClass('alert alert-danger').html('Invalid email/password combination');
+  $('input[type="password"]').val('');
+  setTimeout(function setTimeout() {
+    $('#loginModal .modal-dialog').removeClass('shake');
+  }, 1000);
+}
 
 @Component({
   selector: 'app-navbar',
@@ -46,5 +78,23 @@ export class NavbarComponent implements OnInit {
 
   logoutClick() {
     this.logoutAction.emit();
+  }
+
+  openLoginModal() {
+    showLoginForm();
+    setTimeout(function show() {
+      $('#loginModal').modal('show');
+    }, 230);
+  }
+
+  openRegisterModal() {
+    showRegisterForm();
+    setTimeout(function show() {
+      $('#loginModal').modal('show');
+    }, 230);
+  }
+
+  loginAjax() {
+    shakeModal();
   }
 }
