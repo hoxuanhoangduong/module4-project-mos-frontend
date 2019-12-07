b45b8b4f338c042c8dbb8448d2ef87f6c0480eee
Ho Duong <hxhduong4394@gmail.com>
2019-12-07 10:37:13 +0700

    Create Artist Component

---
+++ src/app/artist/artist.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {ArtistRoutingModule} from './artist-routing.module';
+import {ArtistComponent} from './artist/artist.component';
+import {ArtistListComponent} from './artist-list/artist-list.component';
+import {ArtistDetailComponent} from './artist-detail/artist-detail.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
+// import {PlaylistModule} from '../playlist/playlist.module';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+
+// import {TranslateModule} from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [ArtistComponent, ArtistListComponent, ArtistDetailComponent],
+  imports: [
+    CommonModule,
+    ArtistRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    // PlaylistModule,
+    InfiniteScrollModule
+    // TranslateModule
+  ]
+})
+export class ArtistModule {
+}
