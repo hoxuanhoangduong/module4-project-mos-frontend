da7f07d3ec454b935ac1cbd5d10a5a4f434b5ee3
Ho Duong <hxhduong4394@gmail.com>
2019-12-09 13:34:41 +0700

    Create uploaded song list Component

---
+++ src/app/user/uploaded-song-list/uploaded-song-list.component.ts
@@ -0,0 +1,73 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Page} from '../../model/page';
+import {Song} from '../../model/song';
+import {Subscription} from 'rxjs';
+// import {Playlist} from '../../models/playlist';
+// import {AddSongToPlaylistComponent} from '../../playlist/add-song-to-playlist/add-song-to-playlist.component';
+import {SongService} from '../../service/song.service';
+import {PlayingQueueService} from '../../service/playing-queue.service';
+
+// import {PlaylistService} from '../../services/playlist.service';
+
+@Component({
+  selector: 'app-uploaded-song-list',
+  templateUrl: './uploaded-song-list.component.html',
+  styleUrls: ['./uploaded-song-list.component.scss']
+})
+export class UploadedSongListComponent implements OnInit, OnDestroy {
+
+  pageNumber: number;
+  pageSize: number;
+  pages: Page[] = [];
+  loading: boolean;
+  subscription: Subscription = new Subscription();
+  private message: string;
+  private songList: Song[];
+
+  // @ViewChild(AddSongToPlaylistComponent, {static: false}) child: AddSongToPlaylistComponent;
+
+  constructor(private songService: SongService, private playingQueueService: PlayingQueueService) {
+  }
+
+  ngOnInit() {
+    // this.loading = true;
+    // this.subscription.add(this.songService.getUserSongList().subscribe(
+    //   result => {
+    //     if (result != null) {
+    //       this.songList = result.content;
+    //       this.songList.forEach((value, index) => {
+    //         this.songList[index].isDisabled = false;
+    //       });
+    //       for (const song of this.songList) {
+    //         this.checkDisabledSong(song);
+    //       }
+    //     }
+    //   }, error => {
+    //     this.message = 'Cannot retrieve song list. Cause: ' + error.songsMessage;
+    //   }, () => {
+    //     this.loading = false;
+    //   }
+    // ));
+  }
+
+  addToPlaying(song: Song, event) {
+    event.stopPropagation();
+    song.isDisabled = true;
+    this.playingQueueService.addToQueue(song);
+  }
+
+  checkDisabledSong(song: Song) {
+    let isDisabled = false;
+    for (const track of this.playingQueueService.currentQueueSubject.value) {
+      if (song.url === track.link) {
+        isDisabled = true;
+        break;
+      }
+    }
+    song.isDisabled = isDisabled;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
